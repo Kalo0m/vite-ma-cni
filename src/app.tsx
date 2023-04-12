796759d977c1be53bf1def2dc40deaeae4cfc6f2
Théo Letouzé <theo.letouze44@gmail.com>
2023-04-12 19:31:54 +0200

    create preact + vite project

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import viteLogo from "/vite.svg";
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return <>coucou</>;
+}
